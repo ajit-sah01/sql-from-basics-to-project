63f8529c5e513b2236e8433d2f8d32dd01910d68
Ajit Kumar Sah <ajitk23192@gmail.com>
2026-01-02 11:30:22 +0530

    Add files via upload
    This folder demonstrates how to insert data into SQL tables using the INSERT INTO statement. It includes examples of inserting multiple records into a table in a single query, helping to understand how structured data is added and managed within a relational database

---
+++ 03_Sql_Folder/03_insert_student_basic.sql
@@ -0,0 +1,10 @@
+-- Select the database where the student table exists
+USE mynilam;
+
+-- Insert multiple records into the student table
+INSERT INTO student (rollno, name, address) VALUES
+(1, 'Nilam', 'Bettiha'),
+(2, 'Ajit', 'Bettiha'),
+(3, 'Ravi', 'Gaya'),
+(4, 'Pooja', 'Muzaffarpur'),
+(5, 'Suman', 'Bhagalpur');
